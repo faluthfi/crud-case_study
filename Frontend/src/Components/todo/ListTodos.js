4287cb4fb69a82972cdc2982e6eba336e95f8727
faluthfi <faikar.al@gmail.com>
2022-02-15 18:56:24 +0700

    minor fix

--- Frontend/src/Components/todo/ListTodos.js
+++ Frontend/src/Components/todo/ListTodos.js
@@ -8,8 +8,30 @@ const baseUrl = process.env.REACT_APP_API_URL;
 
 const ListTodos = ({ accessToken }) => {
   const [todos, setTodos] = useState([]);
+  const [description, setDescription] = useState("");
 
-  //delete todo function
+
+  const onSubmitForm = async e => {
+    e.preventDefault();
+    try {
+      const response = await 
+      axios.post(`${baseUrl}/todo/create`,
+      {
+        description
+      },
+      {
+        headers:{
+          Authorization:accessToken
+        },
+      })
+      .then((res)=>{
+        getTodos()
+        alert(res.data.message)
+      })
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
 
   const deleteTodo = async (deleteid) => {
     try {
@@ -24,7 +46,6 @@ const ListTodos = ({ accessToken }) => {
             },
           })
           .then((res) => {
-            console.log(res)
             alert(res.data.message)
           })
       setTodos(todos.filter(todo => todo.id !== deleteid));
@@ -43,7 +64,6 @@ const ListTodos = ({ accessToken }) => {
             },
           })
           .then((res) => {
-            console.log(res)
             setTodos(res.data.data)
           })
     } catch (err) {
@@ -64,7 +84,16 @@ const ListTodos = ({ accessToken }) => {
 
   return (
     <Fragment>
-      {" "}
+      <h1 className="text-center mt-5">Todo List</h1>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+        />
+        <button className="btn btn-success">Add</button>
+      </form>
       <table class="table mt-5 text-center">
         <thead>
           <tr>
--- Frontend/src/Components/todo/index.js
+++ Frontend/src/Components/todo/index.js
@@ -2,14 +2,12 @@ import React, { Fragment } from "react";
 
 //components
 
-import InputTodo from "./InputTodo";
 import ListTodos from "./ListTodos";
 
 function App({accessToken}) {
   return (
     <Fragment>
       <div className="container">
-        <InputTodo accessToken={accessToken}/>
         <ListTodos accessToken={accessToken}/>
       </div>
     </Fragment>
